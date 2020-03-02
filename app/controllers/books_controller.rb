2895bdf9d05e8f73868c0f6e793a8f93a53bf935
fudasatomi <imotas.aduf@gmail.com>
2020-03-02 13:39:32 +0900

    book/show&index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,26 @@
 class BooksController < ApplicationController
+  before_action :access_user,:only=>[:edit ,:update, :destroy]
+    def access_user
+      @book = Book.find(params[:id])
+      if @book.user_id != current_user.id
+       redirect_to books_path
+      end
+    end
 
   def show
+    @booknew = Book.new
   	@book = Book.find(params[:id])
   end
 
   def index
+    @book = Book.new
   	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
   end
 
   def create
   	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
-  	if @book.save #入力されたデータをdbに保存する。
+  	@book.user_id = current_user.id
+    if @book.save #入力されたデータをdbに保存する。
   		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
   	else
   		@books = Book.all
@@ -33,16 +43,16 @@ class BooksController < ApplicationController
   	end
   end
 
-  def delete
-  	@book = Book.find(params[:id])
-  	@book.destoy
+  def destroy
+  	book = Book.find(params[:id])
+    book.destroy
   	redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
 
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title, :body)
   end
 
 end
